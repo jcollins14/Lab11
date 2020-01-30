87dafb1f49bbf1db6acc8c40f68a705992fc8db3
Jake Collins <jakecollins1014@gmail.com>
2020-01-29 19:52:06 -0500

    fixed namespace

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -4,162 +4,162 @@ using System.Linq;
 
 namespace Lab11
 {
-    
-}
-public class Movie
-{
-    public string Title { get; set; }
-    public string Genre { get; set; }
-    public Movie(string title, string genre)
+
+    public class Movie
     {
-        Title = title;
-        Genre = genre;
-    }
-static void Main(string[] args)
-    {
-        bool loop = true;
-
-        //Movie Object Declaration/Instantiation
-        Movie weathering = new Movie("Weathering with You (2019)", "Animation");
-        Movie voice = new Movie("A Silent Voice (2016)", "Animation");
-        Movie gumby = new Movie("Gumby: The Movie (1995)", "Animation");
-        Movie lotr1 = new Movie("Lord of the Rings: Fellowship of the Ring (2001)", "Fantasy");
-        Movie lotr2 = new Movie("Lord of the Rings: The Two Towers (2002)", "Fantasy");
-        Movie lotr3 = new Movie("Lord of the Rings: The Return of the King (2003)", "Fantasy");
-        Movie thing = new Movie("The Thing (1982)", "Horror");
-        Movie shining = new Movie("The Shining (1980)", "Horror");
-        Movie mandy = new Movie("Mandy (2018)", "Horror");
-        Movie blade = new Movie("Blade Runner (1982)", "SciFi");
-        Movie district = new Movie("District 9 (2009)", "SciFi");
-        Movie primer = new Movie("Primer (2004)", "SciFi");
-
-        //Movie Array Declaration
-        Movie[] movies = new Movie[12] { weathering, voice, gumby, lotr1, lotr2, lotr3, thing, shining, mandy, blade, district, primer };
-
-        while (loop)
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public Movie(string title, string genre)
         {
-            Console.WriteLine("Welcome to the Movie Database.");
-            string input = SelectGenre();
+            Title = title;
+            Genre = genre;
+        }
+        static void Main(string[] args)
+        {
+            bool loop = true;
+
+            //Movie Object Declaration/Instantiation
+            Movie weathering = new Movie("Weathering with You (2019)", "Animation");
+            Movie voice = new Movie("A Silent Voice (2016)", "Animation");
+            Movie gumby = new Movie("Gumby: The Movie (1995)", "Animation");
+            Movie lotr1 = new Movie("Lord of the Rings: Fellowship of the Ring (2001)", "Fantasy");
+            Movie lotr2 = new Movie("Lord of the Rings: The Two Towers (2002)", "Fantasy");
+            Movie lotr3 = new Movie("Lord of the Rings: The Return of the King (2003)", "Fantasy");
+            Movie thing = new Movie("The Thing (1982)", "Horror");
+            Movie shining = new Movie("The Shining (1980)", "Horror");
+            Movie mandy = new Movie("Mandy (2018)", "Horror");
+            Movie blade = new Movie("Blade Runner (1982)", "SciFi");
+            Movie district = new Movie("District 9 (2009)", "SciFi");
+            Movie primer = new Movie("Primer (2004)", "SciFi");
+
+            //Movie Array Declaration
+            Movie[] movies = new Movie[12] { weathering, voice, gumby, lotr1, lotr2, lotr3, thing, shining, mandy, blade, district, primer };
+
+            while (loop)
+            {
+                Console.WriteLine("Welcome to the Movie Database.");
+                string input = SelectGenre();
+
+                while (input == "error")
+                {
+                    input = SelectGenre();
+                }
+
+                List<string> movieTitles = new List<string>();
+
+                //add searched genre to list IF it is not already present
+                foreach (Movie movie in movies)
+                {
+                    if (input == movie.Genre)
+                    {
+                        if (!movieTitles.Contains(movie.Title))
+                        {
+                            movieTitles.Add(movie.Title);
+                        }
+                    }
+                }
+
+                //remove previously searched genres from list
+                foreach (Movie movie in movies)
+                {
+                    if (input != movie.Genre)
+                    {
+                        movieTitles.Remove(movie.Title);
+                    }
+                }
+
+                movieTitles.Sort();
+
+                Console.WriteLine();
+                Console.WriteLine("Displaying Results Now:");
+                Console.WriteLine("=======================");
+
+                foreach (string title in movieTitles)
+                {
+                    Console.WriteLine(title);
+                }
+
+                Console.WriteLine();
+                loop = AskLoop();
+            }
+
+            Console.WriteLine("Thank you for using the Movie Database. Goodbye");
+            Environment.Exit(1);
+        }
+
+        public static bool AskLoop()
+        {
+            string input = "error";
+            bool loop = false;
 
             while (input == "error")
             {
-                input = SelectGenre();
-            }
+                Console.WriteLine("Would you like to search again? (yes/no)");
+                input = Console.ReadLine();
+                input.ToLower().Trim();
 
-            List<string> movieTitles = new List<string>();
-
-            //add searched genre to list IF it is not already present
-            foreach (Movie movie in movies)
-            {
-                if (input == movie.Genre)
+                if (input != "yes" && input != "no")
                 {
-                    if (!movieTitles.Contains(movie.Title))
-                    {
-                        movieTitles.Add(movie.Title);
-                    }
+                    Console.WriteLine();
+                    input = "error";
                 }
             }
 
-            //remove previously searched genres from list
-            foreach (Movie movie in movies)
-            {
-                if (input != movie.Genre)
-                {
-                    movieTitles.Remove(movie.Title);
-                }
-            }
-
-            movieTitles.Sort();
-
-            Console.WriteLine();
-            Console.WriteLine("Displaying Results Now:");
-            Console.WriteLine("=======================");
-
-            foreach (string title in movieTitles)
-            {
-                Console.WriteLine(title);
-            }
-
-            Console.WriteLine();
-            loop = AskLoop();
-        }
-
-        Console.WriteLine("Thank you for using the Movie Database. Goodbye");
-        Environment.Exit(1);
-    }
-
-    public static bool AskLoop()
-    {
-        string input = "error";
-        bool loop = false;
-       
-        while (input == "error")
-        {
-            Console.WriteLine("Would you like to search again? (yes/no)");
-            input = Console.ReadLine();
-            input.ToLower().Trim();
-
-            if (input != "yes" && input != "no")
+            if (input == "yes")
             {
                 Console.WriteLine();
-                input = "error";
+                loop = true;
             }
+
+            return loop;
         }
 
-        if (input == "yes")
+        public static string SelectGenre()
         {
-            Console.WriteLine();
-            loop = true;
-        }
+            string answer = "error";
+            int compare = 0;
 
-        return loop;
-    }
+            Console.WriteLine("Please select a genre of movie to search for:");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("1: Animation");
+            Console.WriteLine("2: Fantasy");
+            Console.WriteLine("3: Horror");
+            Console.WriteLine("4: SciFi");
+            Console.WriteLine("Please select an input based on its assigned number.");
 
-    public static string SelectGenre()
-    {
-        string answer = "error";
-        int compare = 0;
+            try
+            {
+                compare = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please input a valid integer for your selection");
+                Console.WriteLine();
+            }
 
-        Console.WriteLine("Please select a genre of movie to search for:");
-        Console.WriteLine("=============================================");
-        Console.WriteLine("1: Animation");
-        Console.WriteLine("2: Fantasy");
-        Console.WriteLine("3: Horror");
-        Console.WriteLine("4: SciFi");
-        Console.WriteLine("Please select an input based on its assigned number.");
+            if (compare == 1)
+            {
+                answer = "Animation";
+            }
+            else if (compare == 2)
+            {
+                answer = "Fantasy";
+            }
+            else if (compare == 3)
+            {
+                answer = "Horror";
+            }
+            else if (compare == 4)
+            {
+                answer = "SciFi";
+            }
+            else
+            {
+                Console.WriteLine("Please select a displayed integer.");
+                Console.WriteLine();
+            }
 
-        try
-        {
-            compare = int.Parse(Console.ReadLine());
+            return answer;
         }
-        catch (FormatException)
-        {
-            Console.WriteLine("Please input a valid integer for your selection");
-            Console.WriteLine();
-        }
-
-        if (compare == 1)
-        {
-            answer = "Animation";
-        }
-        else if (compare == 2)
-        {
-            answer = "Fantasy";
-        }
-        else if (compare == 3)
-        {
-            answer = "Horror";
-        }
-        else if (compare == 4)
-        {
-            answer = "SciFi";
-        }
-        else
-        {
-            Console.WriteLine("Please select a displayed integer.");
-            Console.WriteLine();
-        }
-
-        return answer;
     }
 }
